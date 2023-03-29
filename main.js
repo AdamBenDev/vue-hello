a4909eaa9b42a7c02844ebdebf66ee36c020ce37
AdamBenDev <adamaya9732@gmail.com>
2023-03-29 06:40:11 +0200

    CreateApp

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Vue OK, Vue");
+
+/* 
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const app = Vue.createApp({
+    data(){
+        return {
+            message: 'Hello Vue!!',
+            image: 'https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg'
+        }
+    }
+});
+
+app.mount('#root');
